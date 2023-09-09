f064c647bb89926d9cb7aeb7d494bb51c773180b
Samvictor12 <samvictor1797@gmail.com>
2023-09-09 22:19:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/en",
-    output: "export",
+    // output: "export",
 }
 
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/en',
-          permanent: true,
-        },
-      ]
-    },
-  }
+module.exports = nextConfig
